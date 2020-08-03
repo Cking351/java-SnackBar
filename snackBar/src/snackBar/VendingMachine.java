c3831c1705aa5a40a8ddaf0a2b9ac35d0e7e0f56
Chris King <cking5836@gmail.com>
2020-08-03 14:55:28 -0400

    VendingMachine created

---
+++ snackBar/src/snackBar/VendingMachine.java
@@ -0,0 +1,29 @@
+package snackBar;
+
+public class VendingMachine {
+    private static int maxId = 0;
+
+    private int id;
+    private String name;
+
+    // Constructor
+    public VendingMachine(String name) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+}
