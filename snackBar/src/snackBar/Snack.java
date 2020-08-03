76f5ff5416d223aa9217e37f5d868d6aaf23df04
Chris King <cking5836@gmail.com>
2020-08-03 15:05:59 -0400

    Added Snack class

---
+++ snackBar/src/snackBar/Snack.java
@@ -0,0 +1,59 @@
+package snackBar;
+
+public class Snack {
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private int quantity;
+    private double cost;
+    private int machineId;
+
+    // Constructor
+    public Snack(String name, int quantity, double cost, int machineId) {
+        maxId++;
+
+        id = maxId;
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.machineId = machineId;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getMachineId() {
+        return machineId;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+    }
+}
