353ebb0011c84bf0584752fb86a4dc2278f5b595
Chris King <cking5836@gmail.com>
2020-08-03 14:50:28 -0400

    Initial commit. Set up file structure

---
+++ snackBar/src/snackBar/Main.java
@@ -0,0 +1,10 @@
+package snackBar;
+
+public class Main {
+    private static void workWithSnacks() {
+        System.out.println("It's working!");
+    }
+    public static void main(String[] args) {
+        workWithSnacks();
+    }
+}
